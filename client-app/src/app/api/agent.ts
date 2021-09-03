f5e251f1a37350b58adde03276819b3f18c79e52
timdekievit <timdekievit@hotmail.com>
2021-09-03 13:21:39 +0200

    End of Section 10

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import { Activity } from '../models/activity';
+import { store } from '../stores/store';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -17,9 +18,16 @@ axios.interceptors.response.use(async response => {
         return response;
 
 }, (error: AxiosError) => {
-    const {data, status} = error.response!;
+    const {data, status, config} = error.response!;
+    console.log(error.response);
     switch (status) {
         case 400:
+            if (typeof data === 'string') {
+                toast.error(data);
+            }
+            if (config.method === 'get' && data.errors.hasOwnProperty('id')) {
+                history.push('/not-found');
+            }
             if (data.errors) {
                 const modalStateErrors = [];
                 for (const key in data.errors) {
@@ -28,8 +36,6 @@ axios.interceptors.response.use(async response => {
                     }
                 }
                 throw modalStateErrors.flat();
-            } else {
-                toast.error(data);
             }
             break;
         case 401:
@@ -39,7 +45,8 @@ axios.interceptors.response.use(async response => {
             history.push('/not-found')
             break; 
         case 500:
-            toast.error('server error');
+            store.commonStore.setServerError(data);
+            history.push('/server-error');
             break;           
     }
     return Promise.reject(error);
