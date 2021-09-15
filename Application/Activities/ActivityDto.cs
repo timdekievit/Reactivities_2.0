885f5187a37923b5b2ff1bc2ac3678e9252ec9b2
s1102579 <s1102579@student.hsleiden.nl>
2021-09-15 22:21:46 +0200

    section 14 troubleshooting

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -13,6 +13,8 @@ namespace Application.Activities
         public string Category { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
-        public ICollection<Profile> Profiles { get; set; }
+        public string HostUsername { get; set; }
+        public bool IsCancelled { get; set; }
+        public ICollection<Profile> Attendees { get; set; }
     }
 }
